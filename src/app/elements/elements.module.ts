aa6f1fcc03f75985b8216401bea117912edb97c7
uspehgp <uspehgp@gmail.com>
2021-09-06 11:36:01 +0300

    change domain model to routed model for element component

--- src/app/elements/elements.module.ts
+++ src/app/elements/elements.module.ts
@@ -1,20 +1,19 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { ElementsRoutingModule } from './elements-routing.module';
-import { ElementsHomeComponent } from './elements-home/elements-home.component';
+import {ElementsRoutingModule} from './elements-routing.module';
+import {ElementsHomeComponent} from './elements-home/elements-home.component';
 
 
 @NgModule({
   declarations: [
     ElementsHomeComponent
   ],
-  exports: [
-    ElementsHomeComponent
-  ],
+  exports: [],
   imports: [
     CommonModule,
     ElementsRoutingModule
   ]
 })
-export class ElementsModule { }
+export class ElementsModule {
+}
