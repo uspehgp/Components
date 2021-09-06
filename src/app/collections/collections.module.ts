96e2850c3aad9c249ffd6f3021d27fa40db0541e
uspehgp <uspehgp@gmail.com>
2021-09-06 11:04:20 +0300

    create some components with modules like domain module

---
+++ src/app/collections/collections.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { CollectionsRoutingModule } from './collections-routing.module';
+import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+
+
+@NgModule({
+  declarations: [
+    CollectionsHomeComponent
+  ],
+  exports: [
+    CollectionsHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    CollectionsRoutingModule
+  ]
+})
+export class CollectionsModule { }
---
+++ src/app/elements/elements.module.ts
@@ -0,0 +1,20 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { ElementsRoutingModule } from './elements-routing.module';
+import { ElementsHomeComponent } from './elements-home/elements-home.component';
+
+
+@NgModule({
+  declarations: [
+    ElementsHomeComponent
+  ],
+  exports: [
+    ElementsHomeComponent
+  ],
+  imports: [
+    CommonModule,
+    ElementsRoutingModule
+  ]
+})
+export class ElementsModule { }
