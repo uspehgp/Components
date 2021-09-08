68f544715c0b73109a2099d939a59b9dc028b66b
uspehgp <uspehgp@gmail.com>
2021-09-08 13:42:43 +0300

    transmit title for divider sub-component from elements-home.component.html via <ng-content>

--- src/app/shared/divider/divider.component.ts
+++ src/app/shared/divider/divider.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-divider',
@@ -6,8 +6,6 @@ import {Component, Input, OnInit} from '@angular/core';
   styleUrls: ['./divider.component.css']
 })
 export class DividerComponent implements OnInit {
-
-  @Input() title = ''
 
   constructor() {
   }
