891f08ef73aebc92549a4b8a2c82e3bfc1e62760
uspehgp <uspehgp@gmail.com>
2021-09-09 16:34:36 +0300

    customise table with new property

--- src/app/collections/collections-home/collections-home.component.ts
+++ src/app/collections/collections-home/collections-home.component.ts
@@ -7,14 +7,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CollectionsHomeComponent implements OnInit {
   data: any = [
-    { name: 'James', age: 24, job: 'Designer' },
-    { name: 'Jill', age: 26, job: 'Engineer' },
-    { name: 'Elyse', age: 25, job: 'Engineer' }
+    { name: 'James', age: 24, job: 'Designer', employed: 'true'  },
+    { name: 'Jill', age: 26, job: 'Engineer', employed: 'false' },
+    { name: 'Elyse', age: 25, job: 'Engineer', employed: 'false' }
   ];
   headers: any = [
     { key: 'name', label: 'Name' },
     { key: 'age', label: 'Age' },
-    { key: 'job', label: 'Job' }
+    { key: 'job', label: 'Job' },
+    {key: 'employed', label: 'Are you employed?'}
   ];
 
   constructor() {}
