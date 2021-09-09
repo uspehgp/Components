bd26e13d3493e35c6d53467a843dbe88cdee9142
uspehgp <uspehgp@gmail.com>
2021-09-09 12:15:28 +0300

    create table sub component in collections

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -1,18 +1,22 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import {NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 
-import { CollectionsRoutingModule } from './collections-routing.module';
-import { CollectionsHomeComponent } from './collections-home/collections-home.component';
+import {CollectionsRoutingModule} from './collections-routing.module';
+import {CollectionsHomeComponent} from './collections-home/collections-home.component';
+import {TableComponent} from './table/table.component';
+import {SharedModule} from "../shared/shared.module";
 
 
 @NgModule({
   declarations: [
-    CollectionsHomeComponent
+    CollectionsHomeComponent,
+    TableComponent
   ],
   exports: [],
   imports: [
     CommonModule,
-    CollectionsRoutingModule
+    CollectionsRoutingModule,
+    SharedModule
   ]
 })
 export class CollectionsModule { }
