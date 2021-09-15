fd1a9d4ebfb56343f234130280dd36e62652a0d7
uspehgp <uspehgp@gmail.com>
2021-09-15 11:34:33 +0300

    create accordion component with extended all content

---
+++ src/app/mods/accordion/accordion.component.ts
@@ -0,0 +1,18 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrls: ['./accordion.component.css']
+})
+export class AccordionComponent implements OnInit {
+
+  @Input() items: any = []
+
+  constructor() {
+  }
+
+  ngOnInit(): void {
+  }
+
+}
--- src/app/mods/mods-home/mods-home.component.ts
+++ src/app/mods/mods-home/mods-home.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-mods-home',
@@ -8,8 +8,23 @@ import { Component, OnInit } from '@angular/core';
 export class ModsHomeComponent implements OnInit {
 
   openModal = false;
+  items: any = [
+    {
+      title: ' What is a dog?',
+      content: 'A dog is a type of domesticated animal. Known for its loyalty and faithfulness, it can be found as a welcome guest in many households across the world.'
+    },
+    {
+      title: 'What kinds of dogs are there?',
+      content: 'There are many breeds of dogs. Each breed varies in size and temperament. Owners often select a breed of dog that they find to be compatible with their own lifestyle and desires from a companion.'
+    },
+    {
+      title: 'How do you acquire a dog?',
+      content: 'Three common ways for a prospective owner to acquire a dog is from pet shops, private owners, or shelters.'
+    },
+  ]
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit(): void {
   }
